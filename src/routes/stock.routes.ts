75577415e8a82142c8052f8549c45ef31c2191b2
Alisson Santos <53794075+AlissonSantos17@users.noreply.github.com>
2021-03-09 16:03:26 -0300

    Update stock.routes.ts

--- src/routes/stock.routes.ts
+++ src/routes/stock.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
 import StockController from '../controllers/StockController';
+import authMiddleware from '../middlewares/authMiddleware';
 
 const router = Router();
 
-router.post('/:id', StockController.create);
-router.get('/:id', StockController.listStock);
+router.post('/:id', authMiddleware, StockController.create);
+router.get('/:id', authMiddleware, StockController.listStock);
 
 export default router
